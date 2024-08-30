6e144624ffb009728e92cb8a843201d4c9ed49e0
Kariaki58 <kariakistephen809@gmail.com>
2024-08-30 04:44:18 +0100

    configuring cookies

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,10 +8,11 @@ import AuthProvider from 'react-auth-kit/AuthProvider'
 
 
 const authStore = createStore({
-  authName:'_auth',
-  authType:'cookie',
+  authName: '_auth',
+  authType: 'cookie',
   cookieDomain: window.location.hostname,
   cookieSecure: window.location.protocol === 'https:',
+  cookieSameSite: 'None',
 });
 
 
