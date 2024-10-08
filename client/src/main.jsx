56e576f3465ef41712e9a6bbe1b77ed354016595
kariaki58 <kariakistephen809@gmail.com>
2024-10-08 12:57:34 +0100

    fixed tailwind issues

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -26,7 +26,7 @@ createRoot(document.getElementById('root')).render(
       <SpeedInsights/>
       <ScrollToTop />
       <AuthProvider store={authStore}>
-        <App />                                 
+        <App />                            
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
