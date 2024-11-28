b5bd9c2c780e0c4c8b691f710e1c2c6dab5945ff
kariaki58 <kariakistephen809@gmail.com>
2024-11-28 10:25:14 +0100

    added analytics

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,13 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { SpeedInsights } from "@vercel/speed-insights/react"
+import { ProductUploadProvider } from './contextApi/ProductContext.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import createStore from 'react-auth-kit/createStore'
 import AuthProvider from 'react-auth-kit/AuthProvider'
 import ScrollToTop from './ScrollToTop'
+
+
 import { Analytics as VercelAnalytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/react"
-import { ProductUploadProvider } from './contextApi/ProductContext.jsx'
 
 
 const authStore = createStore({
